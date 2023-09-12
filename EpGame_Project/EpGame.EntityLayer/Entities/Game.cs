1ebc35e43ea118a63cc50f4e64dc787a91434950
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:34:23 +0300

    Create Game.cs

---
+++ EpGame_Project/EpGame.EntityLayer/Entities/Game.cs
@@ -0,0 +1,25 @@
+﻿using EpGame.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.EntityLayer.Entities
+{
+	public class Game : BaseEntity
+	{
+		public string GameName { get; set; }
+		public double Price { get; set; }
+		public DateTime PublishDate { get; set; }
+		public string Description { get; set; }
+		public double Size { get; set; }
+		public double Rating { get; set; }
+		public string SystemRequirements { get; set; }
+		public int DownloadCount { get; set; }
+		public virtual ICollection<Cart> Cart { get; set; }
+		public virtual ICollection<GameCategory> GameCategory { get; set; }
+		public virtual ICollection<Media> Media { get; set; }
+		public virtual ICollection<Gift> Gift { get; set; }
+	}
+}
