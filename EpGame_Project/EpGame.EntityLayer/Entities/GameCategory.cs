3b861c176a8cae340f907569ca63a4cb55b4239f
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:34:27 +0300

    Create GameCategory.cs

---
+++ EpGame_Project/EpGame.EntityLayer/Entities/GameCategory.cs
@@ -0,0 +1,18 @@
+﻿using EpGame.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.EntityLayer.Entities
+{
+	public class GameCategory : IBaseEntity
+	{
+		public int ID { get; set; }
+		public int GameID { get; set; }
+		public int CategoryID { get; set; }
+		public Game Games { get; set; }
+		public Category Categories { get; set; }
+	}
+}
