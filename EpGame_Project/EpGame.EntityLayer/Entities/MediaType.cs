763e6bbd134b9bfbee1ec1529d13927f8f5c8db9
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:34:40 +0300

    Create MediaType.cs

---
+++ EpGame_Project/EpGame.EntityLayer/Entities/MediaType.cs
@@ -0,0 +1,16 @@
+﻿using EpGame.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.EntityLayer.Entities
+{
+	public class MediaType:BaseEntity
+	{
+        public string MediaTypeName { get; set; }
+        public double MediaSize { get; set; }
+        public virtual ICollection<Media> Media { get; set; }
+    }
+}
