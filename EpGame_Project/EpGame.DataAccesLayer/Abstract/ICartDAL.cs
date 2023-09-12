25604e0b8f3742db3e1c2924d57e1c9af8abeebb
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:35:19 +0300

    Create ICartDAL.cs

---
+++ EpGame_Project/EpGame.DataAccesLayer/Abstract/ICartDAL.cs
@@ -0,0 +1,14 @@
+﻿using EpGame.Core.DataAccesLayer;
+using EpGame.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.DataAccesLayer.Abstract
+{
+	public interface ICartDAL:IRepository<Cart>
+	{
+	}
+}
