43fa677b0dd00dd5d018bd3308692641df64bf36
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:36:43 +0300

    Create EFGame.cs

---
+++ EpGame_Project/EpGame.DataAccesLayer/Concrete/ENTITY FRAMEWORK/EFGame.cs
@@ -0,0 +1,73 @@
+﻿using EpGame.DataAccesLayer.Abstract;
+using EpGame.EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.DataAccesLayer.Concrete.ENTITY_FRAMEWORK
+{
+	public class EFGame : IGameDAL
+	{
+		private readonly EpGameDbContext _context;
+
+		public EFGame(EpGameDbContext context)
+		{
+			_context = context;
+		}
+
+		public bool Add(Game entity)
+		{
+			try
+			{
+				_context.Games.Add(entity);
+				_context.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public bool Delete(Game entity)
+		{
+			try
+			{
+				_context.Games.Remove(entity);
+				_context.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public Game Get(int id)
+		{
+			return _context.Games.FirstOrDefault(g => g.ID == id);
+		}
+
+		public List<Game> GetAll()
+		{
+			return _context.Games.ToList();
+		}
+
+		public bool Update(Game entity)
+		{
+			try
+			{
+				_context.Entry(entity).State = EntityState.Modified;
+				_context.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+	}
+}
