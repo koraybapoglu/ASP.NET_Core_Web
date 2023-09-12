b224d6218d97fa06f4a61ac9374344955ca913d4
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:38:13 +0300

    Create MediaTypeMap.cs

---
+++ EpGame_Project/EpGame.DataAccesLayer/Mappings/MediaTypeMap.cs
@@ -0,0 +1,19 @@
+﻿using EpGame.EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.DataAccesLayer.Mappings
+{
+	public class MediaTypeMap : IEntityTypeConfiguration<MediaType>
+	{
+		public void Configure(EntityTypeBuilder<MediaType> builder)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
