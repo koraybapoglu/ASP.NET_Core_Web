8ede86da13be652cdd5a2cf1f733bcf646c99d7c
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:34:36 +0300

    Create Media.cs

---
+++ EpGame_Project/EpGame.EntityLayer/Entities/Media.cs
@@ -0,0 +1,19 @@
+﻿using EpGame.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.EntityLayer.Entities
+{
+	public class Media : BaseEntity
+	{
+		public string MediaURL { get; set; }
+		public int MediaTypeID { get; set; }
+        public int GameID { get; set; }
+        public string AltText { get; set; }
+        public virtual Game Game { get; set; }
+        public virtual MediaType MediaType { get; set; }
+    }
+}
