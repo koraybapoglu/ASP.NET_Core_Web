ef8e9e4495021f6885f15d5a90933a332e7b5d68
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:34:11 +0300

    Create AppUser.cs

---
+++ EpGame_Project/EpGame.EntityLayer/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.EntityLayer.Entities
+{
+	public class AppUser:IdentityUser<int>
+	{
+		public virtual ICollection<Cart> Cart { get; set; }
+	}
+}
