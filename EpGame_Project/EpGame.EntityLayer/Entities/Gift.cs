4349da8bea440445a85b1cbea459950680583531
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:34:31 +0300

    Create Gift.cs

---
+++ EpGame_Project/EpGame.EntityLayer/Entities/Gift.cs
@@ -0,0 +1,17 @@
+﻿using EpGame.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.EntityLayer.Entities
+{
+	public class Gift : BaseEntity
+	{
+		public int GameID { get; set; }
+		public int SendUserID { get; set; }
+		public int ReceiverUserID { get; set; }
+		public virtual Game Games { get; set; }
+	}
+}
