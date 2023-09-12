db0cde8df7ca0e39f9fa1017b1c4b0764f2e8d50
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:34:16 +0300

    Create Cart.cs

---
+++ EpGame_Project/EpGame.EntityLayer/Entities/Cart.cs
@@ -0,0 +1,18 @@
+﻿using EpGame.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.EntityLayer.Entities
+{
+	public class Cart : BaseEntity
+	{
+		public int GameID { get; set; }
+		public int AppUserID { get; set; }
+		public double TotalPrice { get; set; }
+		public virtual Game Game { get; set; }
+		public virtual AppUser AppUser { get; set; }
+	}
+}
