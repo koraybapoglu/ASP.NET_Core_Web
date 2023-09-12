478aac8e6408ea1158a89ff07cce02943973b735
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-12 20:33:25 +0300

    Create IRepository.cs

---
+++ EpGame_Project/EpGame.Core/DataAccesLayer/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using EpGame.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.Core.DataAccesLayer
+{
+	public interface IRepository<TEntity> where TEntity : class,IBaseEntity,new()
+	{
+		bool Add(TEntity entity);
+		bool Delete(TEntity entity);
+		bool Update(TEntity entity);
+		List<TEntity> GetAll();
+		TEntity Get(int id);
+	}
+}
