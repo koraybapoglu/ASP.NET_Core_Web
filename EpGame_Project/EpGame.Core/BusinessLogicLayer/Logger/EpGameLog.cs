86616165c30828eb01c1802ae227c5c6c78ee5fd
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-29 00:21:45 +0300

    Create EpGameLog.cs

---
+++ EpGame_Project/EpGame.Core/BusinessLogicLayer/Logger/EpGameLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.Core.BusinessLogicLayer.Logger
+{
+	public class EpGameLog
+	{
+		public static void AddLog(string message,LogType Logtype) 
+		{
+			FileStream file = new FileStream("EpGameLogs.txt", FileMode.Append);
+			StreamWriter writer = new StreamWriter(file);
+			writer.WriteLine("Log Türü: " + Logtype + " Hata Mesajı: " + message + " Log Tarihi:" + DateTime.Now);
+			writer.Close();
+		}
+	}
+	public enum LogType 
+	{
+		Error,
+		Warning,
+		Info,
+		Insert,
+		Update,
+		Delete,
+		NotFound,
+		NonValidation
+	}
+}
