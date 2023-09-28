1cb929682e9782c5aeea5c558ba2e9dd6f689900
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-28 22:15:04 +0300

    Create CategoryApiController.cs

---
+++ EpGame_Project/Epgame.WebAPI/Controllers/CategoryApiController.cs
@@ -0,0 +1,47 @@
+﻿using EpGame.BusinessLogicLayer.Abstract;
+using EpGame.EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpGame.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoryApiController : Controller
+	{
+		ICategoryService categoryservice;
+		public CategoryApiController(ICategoryService categoryService)
+		{
+			categoryservice = categoryService;
+		}
+		[HttpGet("getallcategories")]
+		public IActionResult GetAll() 
+		{
+			var category=categoryservice.GetAll();
+			return Ok(category);
+		}
+		[HttpGet("getcategory")]
+		public IActionResult Get(int id) 
+		{
+			var category=categoryservice.Get(id);
+			return Ok(category);
+		}
+		[HttpPost("addcategory")]
+		public IActionResult AddCategory(Category category)
+		{
+			var result = categoryservice.Add(category);
+			return Ok(result);
+		}
+		[HttpPost("updatecategory")]
+		public IActionResult UpdateCategory(Category category) 
+		{
+			var result = categoryservice.Update(category);
+			return Ok(result);
+		}
+		[HttpPost("deletecategory")]
+		public IActionResult DeleteCategory(Category category) 
+		{
+			var result = categoryservice.Delete(category);
+			return Ok(result);
+		}
+	}
+}
