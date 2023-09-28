75eca18159f4c9509a7a819334b50f97d0a56ebb
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-28 22:14:59 +0300

    Create CartApiController.cs

---
+++ EpGame_Project/Epgame.WebAPI/Controllers/CartApiController.cs
@@ -0,0 +1,47 @@
+﻿using EpGame.BusinessLogicLayer.Abstract;
+using EpGame.EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpGame.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CartApiController : Controller
+	{
+		ICartService cartService;
+        public CartApiController(ICartService cartservice)
+        {
+				cartService= cartservice;
+        }
+		[HttpGet("getcarts")]
+		public IActionResult GetCarts() 
+		{
+			var carts = cartService.GetAll();
+			return Ok(carts);
+		}
+		[HttpGet("getcart/{id}")]
+		public IActionResult GetCart(int id) 
+		{
+			var cart = cartService.Get(id);
+			return Ok(cart);
+		}
+		[HttpPost("addcart")]
+		public IActionResult Addcart(Cart cart) 
+		{
+			var result = cartService.Add(cart);
+			return Ok(result);
+		}
+		[HttpPost("updatecart")]
+		public IActionResult Updatecart(Cart cart) 
+		{
+			var result = cartService.Update(cart);
+			return Ok(result);
+		}
+		[HttpPost("deletcart")]
+		public IActionResult Deletecart(Cart cart) 
+		{
+			var result= cartService.Delete(cart);
+			return Ok(result);
+		}
+    }
+}
