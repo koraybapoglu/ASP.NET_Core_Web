996406e324f6cad3c9e18bd5dce72fbfaa6ae51a
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-28 22:15:22 +0300

    Create MediaApiController.cs

---
+++ EpGame_Project/Epgame.WebAPI/Controllers/MediaApiController.cs
@@ -0,0 +1,41 @@
+﻿using EpGame.BusinessLogicLayer.Abstract;
+using EpGame.EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpGame.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MediaApiController : Controller
+	{
+		IMediaService mediaService;
+        public MediaApiController(IMediaService MediaService)
+        {
+				mediaService = MediaService;
+        }
+		[HttpGet("GetAllMedias")]
+		public IActionResult GetAll() 
+		{
+			var Medias = mediaService.GetAll();
+			return Ok(Medias);
+		}
+		[HttpGet("GetMedia")]
+		public IActionResult Get(int id) 
+		{
+			var Media=mediaService.Get(id);
+			return Ok(Media);
+		}
+		[HttpPost("UpdateMedia")]
+		public IActionResult UpdateMedia(Media media) 
+		{
+			var result=mediaService.Update(media);
+			return Ok(result);
+		}
+		[HttpPost("DeleteMedia")]
+		public IActionResult DeleteMedia(Media media) 
+		{
+			var result= mediaService.Delete(media);
+			return Ok(result);
+		}
+    }
+}
