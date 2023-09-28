b25aaf63b49df389e6ebda5f84136be7d892d267
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-28 22:15:26 +0300

    Create MediaTypeApiController.cs

---
+++ EpGame_Project/Epgame.WebAPI/Controllers/MediaTypeApiController.cs
@@ -0,0 +1,42 @@
+﻿using EpGame.BusinessLogicLayer.Abstract;
+using EpGame.EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EpGame.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MediaTypeApiController : Controller
+	{
+		IMediaTypeService mediaTypeService;
+		public MediaTypeApiController(IMediaTypeService MediaTypeService)
+		{
+			mediaTypeService = MediaTypeService;
+		}
+		[HttpGet("GetAllMediaTypes")]
+		public IActionResult GetAllMediaTypes()
+		{
+			var mediatypes = mediaTypeService.GetAll();
+			return Ok(mediatypes);
+		}
+		[HttpGet("GetMediaType")]
+		public IActionResult GetMediaType(int id)
+		{
+			var mediaType = mediaTypeService.Get(id);
+			return Ok(mediaType);
+		}
+		[HttpPost("UpdateMedia")]
+		public IActionResult UpdateMediaType(MediaType mediatype)
+		{
+			var result = mediaTypeService.Update(mediatype);
+			return Ok(result);
+		}
+		[HttpPost("DeleteMedia")]
+		public IActionResult DeleteMedia(MediaType mediaType) 
+		{
+			var result = mediaTypeService.Delete(mediaType);
+			return Ok(result);
+		}
+	}
+}
