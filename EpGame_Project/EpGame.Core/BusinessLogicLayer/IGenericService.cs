3733266065f4ea760ac5d19b39d3b80fd9dc8acb
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-29 00:21:50 +0300

    Create IGenericService.cs

---
+++ EpGame_Project/EpGame.Core/BusinessLogicLayer/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using EpGame.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.Core.BusinessLogicLayer
+{
+	public interface IGenericService<TEntity> where TEntity : class,IBaseEntity,new()
+	{
+        bool Add(TEntity entity);
+		bool Delete(TEntity entity);
+		bool Update(TEntity entity);
+		IEnumerable<TEntity> GetAll();
+		TEntity Get(int id);
+    }
+}
