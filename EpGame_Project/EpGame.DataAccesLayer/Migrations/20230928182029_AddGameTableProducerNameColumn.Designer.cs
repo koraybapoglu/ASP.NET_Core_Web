23dac36574e8c96a3caf9b26dc85fd5251ecf26e
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-28 22:16:30 +0300

    UpdatedMigration

--- EpGame_Project/EpGame.DataAccesLayer/Migrations/20230928182029_AddGameTableProducerNameColumn.Designer.cs
+++ EpGame_Project/EpGame.DataAccesLayer/Migrations/20230928182029_AddGameTableProducerNameColumn.Designer.cs
@@ -4,6 +4,7 @@ using EpGame.DataAccesLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EpGame.DataAccesLayer.Migrations
 {
     [DbContext(typeof(EpGameDbContext))]
-    partial class EpGameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230928182029_AddGameTableProducerNameColumn")]
+    partial class AddGameTableProducerNameColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EpGame_Project/EpGame.DataAccesLayer/Migrations/20230928182029_AddGameTableProducerNameColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EpGame.DataAccesLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameTableProducerNameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProducerName",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProducerName",
+                table: "Games");
+        }
+    }
+}
