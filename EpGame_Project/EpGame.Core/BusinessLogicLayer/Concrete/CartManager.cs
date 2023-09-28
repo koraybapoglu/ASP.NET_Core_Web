57cc0ab4023ec72bbb07c6295e65e215dae7c5a8
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-29 00:16:49 +0300

    Create CartManager.cs

---
+++ EpGame_Project/EpGame.Core/BusinessLogicLayer/Concrete/CartManager.cs
@@ -0,0 +1,86 @@
+﻿using EpGame.BusinessLogicLayer.Abstract;
+using EpGame.Core.BusinessLogicLayer.Logger;
+using EpGame.DataAccesLayer.Abstract;
+using EpGame.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpGame.BusinessLogicLayer.Concrete
+{
+	public class CartManager : ICartService
+	{
+		ICartDAL daL;
+        public CartManager(ICartDAL CartDAL)
+        {
+			daL = CartDAL;
+        }
+        public bool Add(Cart entity)
+		{
+			try
+			{
+				return daL.Add(entity);
+			}
+			catch (Exception ex)
+			{
+				EpGameLog.AddLog(ex.Message, LogType.Error);
+				return false;
+			}
+		}
+
+		public bool Delete(Cart entity)
+		{
+			try
+			{
+				return daL.Delete(entity);
+			}
+			catch (Exception ex)
+			{
+				EpGameLog.AddLog(ex.Message, LogType.Error);
+				return false;
+			}
+		}
+
+		public Cart Get(int id)
+		{
+			try
+			{
+				return daL.Get(id);
+			}
+			catch (Exception ex)
+			{
+				EpGameLog.AddLog(ex.Message, LogType.Error);
+				return null;
+			}
+			
+		}
+
+		public IEnumerable<Cart> GetAll()
+		{
+			try
+			{
+				return daL.GetAll();
+			}
+			catch (Exception ex)
+			{
+				EpGameLog.AddLog(ex.Message, LogType.Error);
+				return null;
+			}
+		}
+
+		public bool Update(Cart entity)
+		{
+			try
+			{
+				return daL.Update(entity);
+			}
+			catch (Exception ex)
+			{
+				EpGameLog.AddLog(ex.Message, LogType.Error);
+				return false;
+			}
+		}
+	}
+}
