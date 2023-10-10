70d5080e457be4005691d686d9095254ec0397b1
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-10 22:13:06 +0300

    Create AdoAdmin.cs

---
+++ First_Project_Admin/DataAccesLayer/Concrete/ADO.NET/AdoAdmin.cs
@@ -0,0 +1,42 @@
+﻿using Admin.EntityLayer.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.DataAccesLayer.Concrete.ADO.NET
+{
+    public class AdoAdmin : IAdminDAL
+    {
+        public bool Add(Admins entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Admins entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Admins Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Admins> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetIdByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Admins entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
