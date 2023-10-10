641bc437afe7d35173fc1262705b618b34ffe235
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-10 22:11:57 +0300

    Create BaseEntity.cs

---
+++ First_Project_Admin/Core/Entity/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.Entity
+{
+    public interface IBaseEntity 
+    {
+
+    }
+    public class BaseEntity:IBaseEntity
+    {
+        public BaseEntity()
+        {
+            Active = true;
+            CreatedTime = DateTime.Now;
+            UpdatedTime = DateTime.Now;
+        }
+        public int ID { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+    }
+   
+}
