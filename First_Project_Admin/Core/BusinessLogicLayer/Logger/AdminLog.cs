d84fb11ed0e8e2d506f532355c986d953ba0c9e7
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-10 22:11:31 +0300

    Create AdminLog.cs

---
+++ First_Project_Admin/Core/BusinessLogicLayer/Logger/AdminLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.BusinessLogicLayer.Logger
+{
+    public class AdminLog
+    {
+        public static void AddLog(string message, LogType logType)
+        {
+            FileStream file = new FileStream("AdminLog.txt", FileMode.Append);
+            StreamWriter sw = new StreamWriter(file);
+            sw.WriteLine("Log Türü : " + logType + " Hata Mesajı : " + message + " Log Tarihi : " + DateTime.Now);
+            sw.Close();
+        }
+    }
+    public enum LogType
+    {
+        Error,
+        Warning,
+        Info,
+        Insert,
+        Update,
+        Delete,
+        NotFound,
+        NonValidation
+    }
+}
