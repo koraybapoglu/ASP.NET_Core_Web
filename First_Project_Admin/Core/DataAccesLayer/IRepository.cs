519155d5d062338721bd2fce9ac5cdec9b018be8
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-10 22:11:49 +0300

    Create IRepository.cs

---
+++ First_Project_Admin/Core/DataAccesLayer/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Admin.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.DataAccesLayer
+{
+    public interface IRepository<TEntity> where TEntity : class,IBaseEntity,new()
+    {
+        bool Add(TEntity entity);
+        bool Delete(TEntity entity);
+        bool Update(TEntity entity);
+        List<TEntity> GetAll();
+        TEntity Get(int id);
+    }
+}
