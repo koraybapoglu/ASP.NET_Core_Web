1cf32d75db8128757fa843e9a180b4e7a6a80d54
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-10 22:13:18 +0300

    Create AdminDBContext.cs

---
+++ First_Project_Admin/DataAccesLayer/Concrete/DbContext/AdminDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Admin.DataAccesLayer.Mappings;
+using Admin.EntityLayer.POCO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Admin.DataAccesLayer
+{
+    public class AdminDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-RN1V6Q9;Database=DbBasicAdmin;Trusted_Connection=True;TrustServerCertificate=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AdminMap).Assembly);
+        }
+        public DbSet<Admins> Admins { get; set; }
+    }
+}
