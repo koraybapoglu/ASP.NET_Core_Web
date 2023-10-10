dd23f340f4aec569b9575117f643929a67cb142e
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-10 22:14:16 +0300

    Create IAdminService.cs

---
+++ First_Project_Admin/BusinessLogicLayer/Abstract/IAdminService.cs
@@ -0,0 +1,15 @@
+﻿using Admin.Core.BusinessLogicLayer;
+using Admin.EntityLayer.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.BusinessLogicLayer.Abstract
+{
+    public interface IAdminService : IGenericService<Admins>
+    { 
+        int GetIdByEmail(string email);
+    }
+}
