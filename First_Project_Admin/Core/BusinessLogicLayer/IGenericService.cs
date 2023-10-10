d84bfeed8a8641caaadfa80e8c2d7d084a5a343b
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-10 22:11:19 +0300

    Create IGenericService.cs

---
+++ First_Project_Admin/Core/BusinessLogicLayer/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using Admin.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.BusinessLogicLayer
+{
+    public interface IGenericService<TEntity> where TEntity : class,IBaseEntity,new()
+    {
+        bool Add(TEntity entity);
+        bool Update(TEntity entity);
+        bool Delete(TEntity entity);
+        List<TEntity> GetAll();
+        TEntity Get(int id);
+    }
+}
