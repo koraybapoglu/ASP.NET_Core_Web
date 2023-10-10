3166569db16d1bb6a2f4abc90119fe6cbf1b6250
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-10 22:15:33 +0300

    api

---
+++ First_Project_Admin/WebApi/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Admin.BusinessLogicLayer.Abstract;
+using Admin.EntityLayer.POCO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : Controller
+    {
+        IAdminService adminservice;
+        public AdminController(IAdminService AdminService)
+        {
+               adminservice = AdminService;
+        }
+        [HttpGet("GetAdmins")]
+        public ActionResult GetAdmins() 
+        {
+            var admins = adminservice.GetAll();
+            return Ok(admins);
+        }
+        [HttpGet("GetAdmin")]
+        public ActionResult GetAdmin(int id) 
+        {
+            var admin = adminservice.Get(id);
+            return Ok(admin);
+        }
+        [HttpPost("addadmin")]
+        public ActionResult AddAdmin(Admins Admin) 
+        {
+            var result = adminservice.Add(Admin);
+            return Ok(result);
+        }
+        [HttpPost("deleteadming")]
+        public ActionResult DeleteAdmin(Admins Admin) 
+        {
+            var result = adminservice.Delete(Admin);
+            return Ok(result);
+        }
+        [HttpPost("updateadmin")]
+        public ActionResult UpdateAdmin(Admins Admin) 
+        {
+            var result = adminservice.Update(Admin);
+            return Ok(result);
+        }
+    }
+}
---
+++ First_Project_Admin/WebApi/Program.cs
@@ -0,0 +1,44 @@
+
+using Admin.BusinessLogicLayer.Abstract;
+using Admin.BusinessLogicLayer.Concrete;
+using Admin.DataAccesLayer;
+using Admin.DataAccesLayer.Concrete.ENTITY;
+
+namespace Admin.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<IAdminDAL, EFAdmin>();
+            builder.Services.AddScoped<IAdminService, AdminManager>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
