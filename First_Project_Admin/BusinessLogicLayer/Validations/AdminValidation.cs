7f6b0f1c5a4fbaa3cab917fe5c4b519c6aaeea6e
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-10 22:14:27 +0300

    Create AdminValidation.cs

---
+++ First_Project_Admin/BusinessLogicLayer/Validations/AdminValidation.cs
@@ -0,0 +1,23 @@
+﻿using Admin.DataAccesLayer;
+using Admin.EntityLayer.POCO;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.BusinessLogicLayer.Validations
+{
+    public class AdminValidation : AbstractValidator<Admins>
+    {
+        IAdminDAL DaL;
+        public AdminValidation(IAdminDAL DAL)
+        {
+            RuleFor(x => x.ID).NotNull().WithMessage("ID Alanı Boş Geçilemez.");
+            RuleFor(x => x.UserName).NotNull().WithMessage("Kullanıcı Adı Boş Geçilemez.");
+            RuleFor(x => x.Password).NotNull().WithMessage("Parola Alanı Boş Geçilemez");
+            DaL = DAL;
+        }
+    }
+}
