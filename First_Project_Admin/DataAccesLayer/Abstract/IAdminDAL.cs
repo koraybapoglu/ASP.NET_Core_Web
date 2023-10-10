3f787a019e0e1b139bb9a1d3c25101b492df85ab
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-10 22:12:44 +0300

    Create IAdminDAL.cs

---
+++ First_Project_Admin/DataAccesLayer/Abstract/IAdminDAL.cs
@@ -0,0 +1,15 @@
+﻿using Admin.Core.DataAccesLayer;
+using Admin.EntityLayer.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.DataAccesLayer
+{
+    public interface IAdminDAL : IRepository<Admins>
+    {
+        int GetIdByEmail(string email);
+    }
+}
