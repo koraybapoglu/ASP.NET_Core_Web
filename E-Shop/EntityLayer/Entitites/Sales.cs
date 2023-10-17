922e6e2cfd3e0bf2de2f436c8c63701544b07e97
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-17 23:15:00 +0300

    Create Sales.cs

---
+++ E-Shop/EntityLayer/Entitites/Sales.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Sales
+    {
+        public int ID { get; set; }
+        [Display(Name = "Ürün")]
+        public int UrunID { get; set; }
+        [Display(Name = "Adet")]
+        public int Quantity { get; set; }
+        [Display(Name = "Fiyat")]
+        public decimal Price { get; set; }
+        [Display(Name = "Tarih")]
+        public DateTime Date { get; set; }
+        [Display(Name = "Resim")]
+        public string Image { get; set; }
+        [Display(Name = "Kullanıcı")]
+        public int UserID { get; set; }
+    }
+}
