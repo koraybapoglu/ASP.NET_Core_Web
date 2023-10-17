6a345d71e8e601ce97b731eae5d42880bdaf1a90
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-17 23:16:32 +0300

    Create SalesRepository.cs

---
+++ E-Shop/DataAccesLayer/BusinessLogicLayer/Concrete/SalesRepository.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLogicLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Concrete
+{
+    public class SalesRepository:GenericRepository<Sales>
+    {
+    }
+}
