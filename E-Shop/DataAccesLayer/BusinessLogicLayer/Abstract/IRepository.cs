5e52321b30e44eaceaab0bca12ae2120883ea102
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-17 23:16:13 +0300

    Create IRepository.cs

---
+++ E-Shop/DataAccesLayer/BusinessLogicLayer/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Abstract
+{
+    public interface IRepository<T> where T : class,new()
+    {
+        List<T> List();
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetByID(int id);
+    }
+}
