4f3d1c5f829554ef6868b1218a756d6bbf025874
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-17 23:15:25 +0300

    Create E-ShopDbContext.cs

---
+++ E-Shop/DataAccesLayer/DbContext/E-ShopDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer
+{
+    public class E_ShopDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-RN1V6Q9;Database=E-Shop;Trusted_Connection=True;TrustServerCertificate=true;");
+        }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sales> Sales { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
