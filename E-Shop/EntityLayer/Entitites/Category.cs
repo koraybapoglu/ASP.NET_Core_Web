692d0dd6d5f9f42a4b3e16689e89511b5ef61be0
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-17 23:14:55 +0300

    Create Category.cs

---
+++ E-Shop/EntityLayer/Entitites/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage ="Boş Geçilemez")] //VALIDATION İSLEMLERİ
+        [Display(Name ="Ad")]
+        [StringLength(50,ErrorMessage ="Max 50 karakter olmalıdır.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")] //VALIDATION İSLEMLERİ
+        [Display(Name = "Açıklama")]
+        [StringLength(50, ErrorMessage = "Max 50 karakter olmalıdır.")]
+        public string Description { get; set; }
+        public virtual List<Product> Products { get; set; }
+    }
+}
