8ea944a6a57d332e5173dd133bc40fee52607e44
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-17 23:17:16 +0300

    Create AdminController.cs

---
+++ E-Shop/E-Shop/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shop.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
