5c730df18a05083425f8c95bad2afbf9fe11926d
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-22 14:14:19 +0300

    Create GameController.cs

---
+++ EpGame_Project/EpGame.MVC/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using Epic217.BusinessLogicLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epic217.MVC.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly IGameService service;
+        public GameController(IGameService gameService)
+        {
+            service = gameService;
+        }
+        public IActionResult Detail(int id)
+        {
+            var result = service.GetGamesWithMediasById(id);
+            return View(result);
+        }
+    }
+}
