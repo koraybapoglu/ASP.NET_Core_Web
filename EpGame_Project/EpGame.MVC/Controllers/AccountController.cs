3e2722e79670e6b21e7f69e8b3d99ede75afea9d
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-22 14:14:09 +0300

    Create AccountController.cs

---
+++ EpGame_Project/EpGame.MVC/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using Epic217.Entity.POCO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epic217.MVC.Controllers
+{
+	public class AccountController : Controller
+	{
+        SignInManager<AppUser> signIn;
+        UserManager<AppUser> userManager;
+        public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            signIn = signInManager;
+        }
+        [HttpGet]
+		public IActionResult SignIn()
+		{
+			return View();
+		}
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(string userName,string password)
+        {
+            var result = signIn.PasswordSignInAsync(userName, password, false, false).Result;
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(string userName, string email, string password)
+        {
+
+            
+          var result =  userManager.CreateAsync(new AppUser { UserName = userName, Email = email, NormalizedEmail = email.ToUpper(), NormalizedUserName = userName.ToUpper()},password).Result;
+            if (result.Succeeded)
+            {
+                return View("SignIn");
+            }
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            AppUser user = signIn.UserManager.FindByNameAsync(User.Identity.Name).Result;
+            IList<string> result = signIn.UserManager.GetRolesAsync(user).Result;
+			return View(result);
+		}
+
+        public IActionResult LogOut()
+        {
+            signIn.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
