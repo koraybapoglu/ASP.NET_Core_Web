973af0fa221acd24a81453549f0d2256960ff7d2
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-22 14:16:00 +0300

    Create ErrorViewModel.cs

---
+++ EpGame_Project/EpGame.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Epic217.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
