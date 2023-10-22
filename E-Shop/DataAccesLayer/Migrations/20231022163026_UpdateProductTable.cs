05eb62745fc93d486dac5950e66fd546d8bf483b
Koray Bapoğlu <koray06_61@hotmail.com>
2023-10-23 00:49:42 +0300

    Create 20231022163026_UpdateProductTable.cs

---
+++ E-Shop/DataAccesLayer/Migrations/20231022163026_UpdateProductTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccesLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Products");
+        }
+    }
+}
