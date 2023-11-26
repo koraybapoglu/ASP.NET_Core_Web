b6e9edf924962bcd84bed834b108969203bf3bd4
Koray Bapoğlu <koray06_61@hotmail.com>
2023-11-26 18:51:19 +0300

    Create ProductController.cs

---
+++ Öğrenim Notları/NonAction ve NonController/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekProje.Models;
+
+namespace OrnekProje.Controllers
+{
+    [NonController]
+    public class ProductController : Controller
+    {   
+        public IActionResult Index()
+        {
+            X();
+            return View();
+        }
+        [NonAction] 
+        public void X() 
+        {
+
+        }
+    }
+}
