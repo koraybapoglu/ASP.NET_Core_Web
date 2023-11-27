0bbeeeb62e5c655bc44c7a6ae7d10de70868fba4
Koray Bapoğlu <koray06_61@hotmail.com>
2023-11-27 20:02:24 +0300

    Create ProductController.cs

---
+++ Öğrenim Notları/UrlHelpers Ve HtmlHelpers/Controllers/ProductController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekProje.Models;
+using OrnekProje.Models.ViewModel;
+using System.Text.Json;
+
+namespace OrnekProje.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product{Id=1,ProductName="A Product",Quantity=10 },
+                new Product{Id=2,ProductName="B Product",Quantity=20 },
+                new Product{Id=3,ProductName="C Product",Quantity=30 }
+            };
+
+            #region Model Bazlı Veri Gönderimi
+            //return View(products);
+            #endregion
+
+            #region Veri Taşıma Kontrolleri
+            #region ViewBag
+            ViewBag.Products = products;
+            #endregion
+
+            #region ViewData
+            ViewData["products"] = products;
+            #endregion
+
+            #region TempData
+            string data =JsonSerializer.Serialize(products);
+            TempData["products"] = data;
+
+            //TempData["x"] = 5;
+            //ViewBag.x = 5;
+            //ViewData["x"] = 5;
+            #endregion
+            #endregion
+            return RedirectToAction("Index2");
+        }
+        public IActionResult Index2() 
+        {
+            //var v1=ViewBag.x;
+            //var v2 = ViewData["x"];
+            //var v3 = TempData["x"];
+            var data = TempData["products"].ToString();
+            List<Product> products=JsonSerializer.Deserialize<List<Product>>(data);
+            return View(products);
+        }
+        public IActionResult GetProducts() 
+        {
+            Product product= new Product
+            {
+                Id = 1,
+                ProductName="A Product",
+                Quantity=15
+            };
+            User user = new User()
+            {
+                Id = 1,
+                Name = "Koray",
+                LastName = "Bapoğlu"
+            };
+            //UserProduct userProduct = new UserProduct
+            //{
+            //    User = user,
+            //    Product = product,
+            //};
+            //return View(userProduct);
+
+            var userProduct = (product,user);
+            ViewBag.Mesaj = "Hello World";
+            return View(userProduct);
+        }
+    }
+}
