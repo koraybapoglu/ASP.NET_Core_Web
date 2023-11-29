5e8ac401e18788a4482af099bf25963e837a1f59
Koray Bapoğlu <koray06_61@hotmail.com>
2023-11-29 19:25:56 +0300

    Create ProductController.cs

---
+++ Öğrenim Notları/Kullanıcıdan Veri Alma- Route Parameter/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekProje.Models;
+using System.Text.Json;
+
+namespace OrnekProje.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+            return View();
+        }
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        public IActionResult VeriAl(string id,string a,string b) 
+        {
+            // https://localhost:7100/product/verial/5
+            // id değerine 5 değeri gelmiş oluyor Route Parameter ile
+            var values =Request.RouteValues;
+            return View();
+        }
+    }
+}
