57f5cc5c4af7cb4b25918197ecedf28fd03273fc
Koray Bapoğlu <koray06_61@hotmail.com>
2023-11-29 16:55:44 +0300

    Update ProductController.cs

--- Öğrenim Notları/Kullanıcıdan Veri Alma-QueryString/Controllers/ProductController.cs
+++ Öğrenim Notları/Kullanıcıdan Veri Alma-QueryString/Controllers/ProductController.cs
@@ -20,7 +20,7 @@ namespace OrnekProje.Controllers
         {
             /* https://localhost:7100/product/createproduct?a=5&b=Mehmet */
             // a=5 b=Mehmet değerini almış oldu
-            var querystring = Request.Query; //Request yapılan endpoint'e query string parametresi eklenmiş mi eklenmemiş mi bununla ilgili bilgi verir.
+            var querystring = Request.QueryString; //Request yapılan endpoint'e query string parametresi eklenmiş mi eklenmemiş mi bununla ilgili bilgi verir.
             var aparameter = Request.Query["a"].ToString(); //A'dan gelen veriyi tutar
             var bparameter= Request.Query["b"].ToString(); //B'den gelen veriyi tutar
             return View();
