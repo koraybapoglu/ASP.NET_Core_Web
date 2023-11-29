97db7ba1b33e57c90a8048eb191756a2120f2eda
Koray Bapoğlu <koray06_61@hotmail.com>
2023-11-30 00:08:53 +0300

    Create ProductController.cs

---
+++ Öğrenim Notları/Kullanıcıdan Veri Alma-Ajax/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekProje.Models;
+using System.Text.Json;
+
+namespace OrnekProje.Controllers
+{
+    public class AjaxData 
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+    }
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+            return View();
+        }
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult VeriAl(AjaxData ajaxData) 
+        {
+            return View();
+        }
+    }
+}
