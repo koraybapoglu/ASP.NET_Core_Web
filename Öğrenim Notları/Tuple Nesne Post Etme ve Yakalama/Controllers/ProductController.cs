9b780712757968b6fd4669675aaab3f5f192612b
Koray Bapoğlu <koray06_61@hotmail.com>
2023-11-30 00:58:40 +0300

    Create ProductController.cs

---
+++ Öğrenim Notları/Tuple Nesne Post Etme ve Yakalama/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekProje.Models;
+using System.Text.Json;
+
+namespace OrnekProje.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+            return View();
+        }
+
+        public IActionResult CreateProduct()
+        {
+            var tuple = (new Product(), new User());
+            return View(tuple);
+        }
+        [HttpPost]
+        public IActionResult CreateProduct([Bind(Prefix="Item1")]Product product,[Bind(Prefix ="Item2")] User user)
+        {
+            return View();
+        }
+    }
+}
