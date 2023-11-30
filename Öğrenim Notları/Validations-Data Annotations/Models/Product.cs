aef453b85d253ff988d6f6c5fd8cb54160002bf5
Koray Bapoğlu <koray06_61@hotmail.com>
2023-11-30 18:33:06 +0300

    Create Product.cs

---
+++ Öğrenim Notları/Validations-Data Annotations/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrnekProje.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage ="Lütfen product name'i giriniz.")]
+        [StringLength(100,ErrorMessage ="Lütfen product name'i en fazla 100 karakter giriniz.")]
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+        [EmailAddress(ErrorMessage ="Lütfen doğru bir email adresi giriniz.")]
+        public string Email { get; set; }
+    }
+}
