5e1296ac49fccd851957ecf948b0c0ec330fc4ab
Koray Bapoğlu <koray06_61@hotmail.com>
2023-12-04 00:15:22 +0300

    Create ProductMetadata.cs

---
+++ Öğrenim Notları/Layout,RenderBody,RenderSection/Models/ModelMetadataTypes/ProductMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrnekProje.Models.ModelMetadataTypes
+{
+    public class ProductMetadata
+    {
+        [Required(ErrorMessage = "Lütfen product name'i giriniz.")]
+        [StringLength(100, ErrorMessage = "Lütfen product name'i en fazla 100 karakter giriniz.")]
+        public string ProductName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Lütfen doğru bir email adresi giriniz.")]
+        public string Email { get; set; }
+    }
+}
