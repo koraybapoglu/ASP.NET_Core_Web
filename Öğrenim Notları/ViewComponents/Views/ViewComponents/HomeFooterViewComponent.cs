13f77c718b1a9e44a1c78f2f6226412e73b6b8c9
Koray Bapoğlu <koray06_61@hotmail.com>
2023-12-06 00:49:03 +0300

    Create HomeFooterViewComponent.cs

---
+++ Öğrenim Notları/ViewComponents/Views/ViewComponents/HomeFooterViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekProje.Models;
+
+namespace OrnekProje.ViewComponents
+{
+    public class HomeFooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            List<FooterData> data = new List<FooterData> {
+            new FooterData { Id=1, Name = "Koray", Surname = "Bapoğlu"},
+            new FooterData { Id=2, Name = "Koray2", Surname = "Bapoğlu2"},
+            new FooterData { Id=3, Name = "Koray3", Surname = "Bapoğlu3"}
+            };
+            return View(data);
+        }
+    }
+}
