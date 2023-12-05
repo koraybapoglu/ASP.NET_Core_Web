85f336d88b5c185f5267958f620895b167f81188
Koray Bapoğlu <koray06_61@hotmail.com>
2023-12-06 00:45:40 +0300

    Create HomeController.cs

---
+++ Öğrenim Notları/ViewComponents/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrnekProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var images = new List<string> { "https://w7.pngwing.com/pngs/969/229/png-transparent-banana-scalable-graphics-s-of-banana-food-fruit-banana.png", "https://png.pngtree.com/png-clipart/20230508/original/pngtree-the-banana-png-image_9149388.png" };
+
+            ViewBag.Data=images;
+            return View(images);
+        }
+        public IActionResult Sayfa1()
+        {
+            return View();
+        }
+        public IActionResult Sayfa2()
+        {
+            return View();
+        }
+        public IActionResult Index5()
+        {
+            return View();
+        }
+    }
+}
