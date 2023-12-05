b95de53024ac574affdb7175a07790b90fc28c6b
Koray Bapoğlu <koray06_61@hotmail.com>
2023-12-06 00:46:23 +0300

    Create User.cs

---
+++ Öğrenim Notları/ViewComponents/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace OrnekProje.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
