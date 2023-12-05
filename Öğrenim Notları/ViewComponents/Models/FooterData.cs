8a5ed4435dd927e4d5621a54ecf418846a9595b5
Koray Bapoğlu <koray06_61@hotmail.com>
2023-12-06 00:46:07 +0300

    Create FooterData.cs

---
+++ Öğrenim Notları/ViewComponents/Models/FooterData.cs
@@ -0,0 +1,9 @@
+﻿namespace OrnekProje.Models
+{
+    public class FooterData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
