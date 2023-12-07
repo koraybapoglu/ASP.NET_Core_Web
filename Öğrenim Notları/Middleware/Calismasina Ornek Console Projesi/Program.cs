ea7d217d35033302d2acbcf3d2ee49508756a485
Koray Bapoğlu <koray06_61@hotmail.com>
2023-12-07 15:10:37 +0300

    Create Program.cs

---
+++ Öğrenim Notları/Middleware/Calismasina Ornek Console Projesi/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MiddlewareCalismasiOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Middleware1();
+            Console.ReadLine();
+        }
+        public static void Middleware1() 
+        {
+            Console.WriteLine("Request Geldi");
+            Console.WriteLine("Middleware 1 Çalıştı !");
+            Dongu("Middleware1");
+            Middleware2();
+            Console.WriteLine("Middleware 1 İşlemi Tamamladı !");
+            Console.WriteLine("Response Döndürüldü !");
+        }
+        public static void Middleware2() 
+        {
+            Console.WriteLine("Middleware2 Çalıştırıldı !");
+            Dongu("Middleware2");
+            Middleware3();
+            Console.WriteLine("Middleware 2 Tamamlandı !");
+        }
+        public static void Middleware3() 
+        {
+            Console.WriteLine("Middleware3 Çalıştırıldı !");
+            Dongu("Middleware3");
+            Console.WriteLine("Middleware 3 Tamamlandı !");
+        }
+        public static void Dongu(string MiddlewareAdi) 
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(MiddlewareAdi+" "+i);
+            }
+        }
+    }
+}
