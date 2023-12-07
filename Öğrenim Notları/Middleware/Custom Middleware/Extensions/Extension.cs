a2cf720ccb88b179f92f8a7a62ae6f23d69e37ab
Koray Bapoğlu <koray06_61@hotmail.com>
2023-12-07 17:52:16 +0300

    Create Extension.cs

---
+++ Öğrenim Notları/Middleware/Custom Middleware/Extensions/Extension.cs
@@ -0,0 +1,12 @@
+﻿using MiddlewareExample.Middlewares;
+
+namespace MiddlewareExample.Extensions
+{
+    static public class Extension
+    {
+        public static IApplicationBuilder UseHello(this IApplicationBuilder app) 
+        {
+            return app.UseMiddleware<HelloMiddleware>();
+        }
+    }
+}
