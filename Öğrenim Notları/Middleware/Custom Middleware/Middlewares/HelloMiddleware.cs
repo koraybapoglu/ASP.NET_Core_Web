cd05cea40c68c3166cb37fd8ca08e7fde30c9c2f
Koray Bapoğlu <koray06_61@hotmail.com>
2023-12-07 17:52:09 +0300

    Create HelloMiddleware.cs

---
+++ Öğrenim Notları/Middleware/Custom Middleware/Middlewares/HelloMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace MiddlewareExample.Middlewares
+{
+    public class HelloMiddleware
+    {
+        RequestDelegate _next;
+        public HelloMiddleware(RequestDelegate next)
+        {
+                _next = next;
+        }
+        public async Task Invoke(HttpContext httpContext) 
+        {
+            //Custom operasyon....
+
+            Console.WriteLine("Hello");
+            await _next.Invoke(httpContext);
+            Console.WriteLine("Hello2");
+        }
+    }
+}
