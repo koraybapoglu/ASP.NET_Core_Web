63d9bd4b6ec3323ba1720a371a6ccd8040e9ccb4
Koray Bapoğlu <koray06_61@hotmail.com>
2023-12-07 16:24:32 +0300

    Create Program.cs

---
+++ Öğrenim Notları/Middleware/Use Methodu/Program.cs
@@ -0,0 +1,44 @@
+namespace MiddlewareExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.Use(async (context, task) => 
+            {
+                Console.WriteLine("Start Use Middleware");
+                await task.Invoke();
+                Console.WriteLine("Stop Use Middleware");
+            }
+            );
+            app.Run(async c =>
+            {
+                Console.WriteLine("Start Run Middleware");
+            });
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
